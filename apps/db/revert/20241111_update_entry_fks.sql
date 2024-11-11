1925b454e99dfc49b4093d2313bd92e67b51add8
Manuel Pacheco <contact@manuelpacheco.dev>
2024-11-10 21:37:17 -0300

    add migration to update entry fks

---
+++ apps/db/revert/20241111_update_entry_fks.sql
@@ -0,0 +1,27 @@
+BEGIN;
+
+-- Drop account_no foreign key constraint
+ALTER TABLE transaction_entry
+    DROP CONSTRAINT transaction_entry_account_no_fkey;
+
+-- Add constraint again with ON UPDATE CASCADE
+ALTER TABLE transaction_entry
+    ADD CONSTRAINT transaction_entry_account_no_fkey
+        FOREIGN KEY (account_no)
+        REFERENCES account (number)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION;
+
+-- Drop transaction_id foreign key constraint
+ALTER TABLE transaction_entry
+    DROP CONSTRAINT transaction_entry_transaction_id_fkey;
+
+-- Add constraint again with ON UPDATE CASCADE ON DELETE CASCADE
+ALTER TABLE transaction_entry
+    ADD CONSTRAINT transaction_entry_transaction_id_fkey
+        FOREIGN KEY (transaction_id)
+        REFERENCES transaction (id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION;
+
+COMMIT;
