c6f2dc2e3adc7224b6e572ef11418b952cf51c70
Manuel Pacheco <contact@manuelpacheco.dev>
2025-01-19 10:03:25 -0300

    rename column in typeorm model usage

--- apps/api/src/app/transactions/controller.ts
+++ apps/api/src/app/transactions/controller.ts
@@ -54,7 +54,7 @@ export class TransactionsController {
             amount: entry.amount.toString(),
 
             transactionId,
-            accountNo: entry.accountNo,
+            accountNumber: entry.accountNo,
           })
         );
 
