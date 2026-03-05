bebe78c90b2486b1d36538adffb04729d79fb809
María Lourdes <marylo281292@gmail.com>
2026-03-05 02:37:00 -0300

    create accounts page

---
+++ apps/web/src/app/accounts/page.tsx
@@ -0,0 +1,14 @@
+import { PageTitle } from '../../components/page-title';
+
+export default function AccountsPage() {
+  return (
+    <div>
+      <PageTitle title="Accounts" />
+    </div>
+  );
+}
+
+export const metadata = {
+  title: 'Accounts | Plutonaut',
+  description: 'Accounts',
+};
