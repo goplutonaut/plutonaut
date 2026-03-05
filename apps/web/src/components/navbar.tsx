0caf8611a5f7a87b9fe58382a18a62c2639b7cf5
María Lourdes <marylo281292@gmail.com>
2026-03-05 02:36:24 -0300

    create MainContainer component

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,22 +1,25 @@
 import Link from 'next/link';
+import { MainContainer } from './main-container';
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between p-6 bg-slate-900 text-white shadow-lg">
-      <div className="text-2xl font-bold bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">
-        Plutonaut
-      </div>
-      <div className="space-x-8">
-        <Link href="/" className="hover:text-blue-400 transition-colors">
-          Home
-        </Link>
-        <Link
-          href="/accounts"
-          className="hover:text-blue-400 transition-colors"
-        >
-          accounts
-        </Link>
-      </div>
+    <nav className="bg-slate-900 text-white shadow-lg">
+      <MainContainer className="p-4 flex items-center justify-between">
+        <div className="text-2xl font-bold bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">
+          Plutonaut
+        </div>
+        <div className="space-x-8">
+          <Link href="/" className="hover:text-blue-400 transition-colors">
+            Home
+          </Link>
+          <Link
+            href="/accounts"
+            className="hover:text-blue-400 transition-colors"
+          >
+            accounts
+          </Link>
+        </div>
+      </MainContainer>
     </nav>
   );
 };
