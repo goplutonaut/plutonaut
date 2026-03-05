2ff4e70b940cb3126562ea41cf8b0ffe6da73e0c
María Lourdes <marylo281292@gmail.com>
2026-03-05 02:36:24 -0300

    create MainContainer component

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
-import './global.css';
+import { MainContainer } from '../components/main-container';
 import { Navbar } from '../components/navbar';
+import './global.css';
 
 export const metadata = {
   title: 'Plutonaut',
@@ -15,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-slate-50 min-h-screen text-slate-900">
         <Navbar />
-        <main className="container mx-auto mt-8 px-4">{children}</main>
+        <MainContainer className="mt-8 px-4">{children}</MainContainer>
       </body>
     </html>
   );
