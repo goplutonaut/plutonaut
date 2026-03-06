33b072c043f8a3696ca7200c982acd9c6cc66103
María Lourdes <marylo281292@gmail.com>
2026-03-06 06:52:31 -0300

    move fetch accounts data for chat of accounts component

--- apps/web/src/app/accounts/page.tsx
+++ apps/web/src/app/accounts/page.tsx
@@ -1,33 +1,13 @@
 import { PageTitle } from '../../components/page-title';
-import { ENV } from '../../config/env.config';
 
-import { Account } from '../../types/account';
 import { ChartOfAccounts } from './components/chart-of-accounts';
 
-const getAccounts = async (): Promise<Account[]> => {
-  try {
-    const res = await fetch(`${ENV.API_URL}/accounts`, {
-      cache: 'no-store',
-    });
-
-    if (!res.ok) throw new Error('Failed to fetch');
-
-    return res.json();
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-};
-
 export default async function AccountsPage() {
-  const accounts = await getAccounts();
-  console.log(accounts);
-
   return (
-    <div>
+    <>
       <PageTitle title="Accounts" />
-      <ChartOfAccounts accounts={accounts} />
-    </div>
+      <ChartOfAccounts />
+    </>
   );
 }
 
