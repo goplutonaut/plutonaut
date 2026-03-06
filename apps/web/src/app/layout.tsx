87b4ed0f789176aa929313bf3733f31d95cbbbe7
María Lourdes <marylo281292@gmail.com>
2026-03-06 06:50:56 -0300

    add providers to the layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { MainContainer } from '../components/main-container';
 import { Navbar } from '../components/navbar';
+import Providers from '../components/providers';
 import './global.css';
 
 export const metadata = {
@@ -16,7 +17,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-slate-50 dark:bg-slate-800 min-h-screen text-slate-900 dark:text-slate-100">
         <Navbar />
-        <MainContainer className="mt-8 px-4">{children}</MainContainer>
+        <Providers>
+          <MainContainer className="mt-8 px-4">{children}</MainContainer>
+        </Providers>
       </body>
     </html>
   );
