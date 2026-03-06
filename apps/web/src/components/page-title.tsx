da035ccc332f464aa61e6915f83d5c303d5b717d
María Lourdes <marylo281292@gmail.com>
2026-03-06 03:36:01 -0300

    update page title margin

--- apps/web/src/components/page-title.tsx
+++ apps/web/src/components/page-title.tsx
@@ -1,6 +1,6 @@
 export const PageTitle = ({ title }: { title: string }) => {
   return (
-    <h1 className="text-3xl md:text-5xl font-bold bg-gradient-to-r from-blue-600 to-purple-400 bg-clip-text text-transparent mb-4">
+    <h1 className="text-3xl md:text-5xl font-bold bg-gradient-to-r from-blue-600 to-purple-400 bg-clip-text text-transparent mb-10">
       {title}
     </h1>
   );
