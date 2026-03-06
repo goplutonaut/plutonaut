981c10b4d87898bf13af7272a76578dd78a2e7db
María Lourdes <marylo281292@gmail.com>
2026-03-06 03:40:15 -0300

    remove redundant classes in section title

--- apps/web/src/components/section-title.tsx
+++ apps/web/src/components/section-title.tsx
@@ -1,7 +1,3 @@
 export const SectionTitle = ({ title }: { title: string }) => {
-  return (
-    <h2 className="text-xl md:text-3xl font-bold text-slate-900 dark:text-white mb-4">
-      {title}
-    </h2>
-  );
+  return <h2 className="text-xl md:text-3xl font-bold mb-4">{title}</h2>;
 };
