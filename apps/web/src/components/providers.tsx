0024810d6f0b69cc595069c68728ce06a593184b
María Lourdes <marylo281292@gmail.com>
2026-03-10 03:49:28 -0300

    add next theme provider

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,12 +1,22 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { useState } from 'react';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </NextThemesProvider>
+    </QueryClientProvider>
   );
 }
