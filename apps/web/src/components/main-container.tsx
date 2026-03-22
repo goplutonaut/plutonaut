a68b124a163aff744587358f95f29d47a1910274
María Lourdes <marylo281292@gmail.com>
2026-03-22 13:37:32 -0300

    update main container

--- apps/web/src/components/main-container.tsx
+++ apps/web/src/components/main-container.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@plutonaut/web/src/lib/utils';
+
 export const MainContainer = ({
   children,
   className,
@@ -5,7 +7,5 @@ export const MainContainer = ({
   children: React.ReactNode;
   className?: string;
 }) => {
-  return (
-    <div className={`max-w-5xl mx-auto ${className || ''}`}>{children}</div>
-  );
+  return <div className={cn('max-w-5xl mx-auto', className)}>{children}</div>;
 };
