7270a94b01b048afaf23230d5e44255c90ca3252
María Lourdes <marylo281292@gmail.com>
2026-05-17 02:22:27 -0400

    refactor(AccountsPage): update import statements for consistency and clarity

--- apps/web/src/app/accounts/page.tsx
+++ apps/web/src/app/accounts/page.tsx
@@ -1,6 +1,5 @@
-import { PageTitle } from '../../components/page-title';
-
-import { ChartOfAccounts } from './components/chart-of-accounts';
+import ChartOfAccounts from '@plutonaut/web/src/app/accounts/components/chart-of-accounts';
+import { PageTitle } from '@plutonaut/web/src/components/page-title';
 
 export default async function AccountsPage() {
   return (
