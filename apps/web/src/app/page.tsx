5d122335331e049bfec9f0797209af30da066400
María Lourdes <marylo281292@gmail.com>
2026-05-17 02:22:38 -0400

    fix(Index): correct welcome message to include project name

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome web 👋
+              Welcome to Plutonaut👋
             </h1>
           </div>
         </div>
